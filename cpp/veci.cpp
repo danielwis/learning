56b61e7216b5917f13e9241491cff4c42619dee5
Daniel Williams <daniel_williams@live.com>
2021-06-18 00:43:43 +0200

    Complete c++ veci

--- cpp/veci.cpp
+++ cpp/veci.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+
+void swap(char *a, char *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 std::string veci(std::string inp){
-    std::vector<int> vecivec;
+    std::vector<char> vecivec;
 
     for(int i = 0; i<inp.size(); i++){
-        vecivec.push_back((int) inp.at(i) - '0');
+        vecivec.push_back((char) inp.at(i));
     }
 
+    // The pivot's index and value
     int pivotidx {};
     int pivotelem {};
+
+    // Go backwards until we find two numbers that are sorted
     for(int i = vecivec.size() - 1; i > 0; i--){
         if (vecivec.at(i) > vecivec.at(i-1)){
             pivotidx = i-1;
             pivotelem = vecivec.at(i-1);
             break;
         }
+
+        // If no two such elements exist - return 0
+        if (i == 1) return "0";
     }
 
-    std::sort(vecivec.begin + pivotidx, vecivec.end); // Sort only the required range
+    std::sort(vecivec.begin() + pivotidx, vecivec.end()); // Sort only the required range
 
-    for(int i = pivotidx; i < vecivec.size(); i++){
-        if (vecivec.at(i) > pivotelem){ // shift this down
-            std::move(vecivec.begin + pivotidx, vecivec.begin + pivotidx, veci
+    int swaps {0}; // Amount of swaps needed to shift pivot down to new position
 
-    return "hi";
+    while (vecivec.at(pivotidx) <= pivotelem){
+        pivotidx++;
+        swaps++;
+    }
+
+    /* Now, we are at the index where the element is the lowest greater than the pivot element,
+     * so swap this lowest greater with the one before until it is at the beginning of the sorted list */
+    while (swaps-- > 0){
+        swap(&vecivec.at(pivotidx), &vecivec.at(pivotidx - 1));
+        pivotidx--;
+    }
+
+    return std::string(vecivec.begin(), vecivec.end());
 }
 
 int main(){
     std::string inp{};
     std::getline(std::cin, inp);
-    std::cout << inp << std::endl;
+    std::cout << veci(inp) << std::endl;
     return 0;
 }
